201e8b83aff4865fda1d3f1cd38388de8586b4c3
Mariusz Polak <s16449@pjwstk.edu.pl>
2020-10-28 23:04:53 +0100

    Cw_1

---
+++ Cw_1/Cw_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cw_1
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var strona = "https://pl.prefa.com/kontakt/panstwa-osoba-do-kontaktu-w-prefa/";
+            var result = await client.GetAsync(strona);
+
+            if (result.IsSuccessStatusCode)
+            {
+                string html = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z0-9]+\\.*[a-z0-9]+@[a-z0-9]+\\.[a-z]+\\.*[a-z]+", RegexOptions.IgnoreCase);
+                var matches = regex.Matches(html);
+                foreach (var m in matches)
+                {
+                    Console.WriteLine("Adres ze strony " + strona + " : " + m);
+                }
+            }
+        }
+    }
+}
